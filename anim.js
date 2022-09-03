3c2ede77ec3d82937fe60e2e1faf12c103f52125
Dwonnyy <104561788+Dwonnyy@users.noreply.github.com>
2022-09-03 17:50:08 +0200

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,6 @@
+gsap.from('.section-two',{ scrollTrigger :'.section-two',opacity : 0, duration : 1.5, y : 100,  delay : 0.5 })
+gsap.from('.section-three',{ scrollTrigger :'.section-three',opacity : 0, duration : 1.5, x : 100, delay : 0.5 })
+gsap.from('.section-four',{ scrollTrigger :'.section-four',opacity : 0, duration : 1.5, y : 100, delay : 0.5 })
+gsap.from('.section-five',{ scrollTrigger :'.section-five',opacity : 0, duration : 1.5, x : 100, delay : 0.5 })
+gsap.from('.section-six',{ scrollTrigger :'.section-six',opacity : 0, duration : 1.5, y : -10, delay : 0.5 })
+gsap.from('.section-seven',{ scrollTrigger :'.section-seven',opacity : 0, duration : 1.5, x : -100, delay : 0.5 })
